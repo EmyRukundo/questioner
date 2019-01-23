6039e605b0f032dec2a02ffb262a922a93bad466
EmyRukundo <rukundoemma@gmail.com>
2019-01-23 21:00:22 +0200

    add get allusers

---
+++ server/controllers/allUsers.js
@@ -0,0 +1,17 @@
+import users from '../models/user';
+import Connection from '../db/connect';
+
+const getUsers = async (req, res) => {
+  if (users) {
+    return res.json({
+      status: 200,
+      data:users,
+    });
+  }
+
+  return res.json({
+    status: 404,
+    error: 'No users found',
+  });
+};
+export default getUsers;
